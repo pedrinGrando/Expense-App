3cf6624f212775dab6ad712e553c80878f22938d
Renan-Leal <renan.leallol@gmail.com>
2024-06-12 00:30:35 -0300

    :refactor: Remoção de console.log desnecessário

--- expenseapi/src/main/java/com/rp/expenseapi/security/JwtTokenUtil.java
+++ expenseapi/src/main/java/com/rp/expenseapi/security/JwtTokenUtil.java
@@ -58,7 +58,6 @@ public class JwtTokenUtil {
   //3. According to JWS Compact Serialization(https://tools.ietf.org/html/draft-ietf-jose-json-web-signature-41#section-3.1)
   //   compaction of the JWT to a URL-safe string
   private String doGenerateToken(Map<String, Object> claims, String subject) {
-    System.out.println("aaaaaaa");
     return Jwts.builder()
         .claims(claims)
         .subject(subject)
